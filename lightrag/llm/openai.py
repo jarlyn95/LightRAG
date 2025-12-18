f1c20539f7e13f0eda81ef878e8e9da6b0732158
jialin <jialin@hiocs.com>
2025-12-18 19:33:04 +0800

    embedding 维度

--- lightrag/llm/openai.py
+++ lightrag/llm/openai.py
@@ -657,7 +657,8 @@ async def openai_embed(
     async with openai_async_client:
         response = await openai_async_client.embeddings.create(
             model=model, input=texts, encoding_format="base64",
-            dimensions=int(os.getenv("EMBEDDING_DIM", "1024")),
+            dimensions=embedding_dim,
+
         )
         return np.array(
             [
